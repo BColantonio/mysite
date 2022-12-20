d61ece949b65d81ea5992f1b6fb38241c5ab52c8
Brian C <brian.j.colantonio@gmail.com>
2022-12-20 04:55:17 -0500

    mvp

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-// import { enableProdMode } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 
 // Enable production mode
-// enableProdMode();
+enableProdMode();
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
